c7bcf98faeafc58c1b68ed2fe0678ffd7c9d2c8d
Yarenhi <yarenhi@gmail.com>
2023-10-18 12:17:49 -0600

    instalacion

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 module.exports = () => {
   // ...
 };
+// const mdLinks = require("md-links");
+
+// mdLinks("./some/example.md")
+//   .then(links => {
+//     // => [{ href, text, file }, ...]
+//   })
+//   .catch(console.error);
+
+//Creamos la promesa 
+const mdLinks = (path) => {
+  return new Promise((resolve, reject) => {
+   
+  });
+};

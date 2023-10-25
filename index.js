e5acf1e3ef6666737beb2397b8238dd73feb3ca6
Yarenhi <yarenhi@gmail.com>
2023-10-25 13:25:03 -0600

    ARchivo markdown

--- index.js
+++ index.js
@@ -12,6 +12,14 @@
 const path = require('path');
 const fs = require('fs').promises;
 
+const mdExtensions = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'];
+
+// Función para verificar si el archivo es de tipo Markdown
+function isMarkdownFile(filePath) {
+  const fileExtension = path.extname(filePath);
+  return mdExtensions.includes(fileExtension);
+}
+
 const mdLinks = (filePath) => {
   return new Promise((resolve, reject) => {
     // Verificamos que la ruta exista
@@ -19,32 +27,42 @@ const mdLinks = (filePath) => {
       .then(() => {
         // La ruta existe, continuamos con el procesamiento
         const absolutePath = path.resolve(filePath); // Convertimos la ruta a absoluta
-        console.log(absolutePath); // Mueve el console.log aquí
-        return fs.readFile(absolutePath, 'utf-8');
+        console.log(absolutePath); // Mueve el console.log aquí para mostrar la ruta absoluta
+
+        // Verificamos si el archivo es de Markdown
+        if (isMarkdownFile(filePath)) {
+          // Leemos el contenido del archivo
+          return fs.readFile(absolutePath, 'utf-8');
+        } else {
+          reject('El archivo no es de tipo Markdown');
+        }
       })
       .then((content) => {
         const links = [];
         const linkRegex = /(\[([^\]]*)\]\(([^)]*)\))/g;
 
         let match;
+        // Encontramos y almacenamos los links en el archivo
         while ((match = linkRegex.exec(content)) !== null) {
           const [, fullMatch, text, href] = match;
           links.push({ href, text, file: filePath });
         }
 
+        // Resolvemos la promesa con la lista de links
         resolve(links);
       })
       .catch((error) => {
-        reject(`Error al leer el archivo: ${error.message}`);
+        // Si hay un error al leer el archivo o no es Markdown, rechazamos la promesa
+        reject(`Error: ${error.message}`);
       });
   });
 };
 
 // Ejemplo de uso
-mdLinks('C:/Users/Yaya/Laboratoria/DEV010-md-links/lib/Readme.md')
+mdLinks('./lib/Readme.md')
   .then((links) => {
-    console.table(links);
+    console.log(links); // Imprime la lista de links
   })
   .catch((error) => {
-    console.error(error);
+    console.error(error); // Maneja cualquier error que pueda ocurrir
   });

9c5db313fd344373722e31ccb7cf01ea576ddbe4
Yarenhi <yarenhi@gmail.com>
2023-10-25 10:07:37 -0600

    hito 1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
 // const mdLinks = require("md-links");
 
 // mdLinks("./some/example.md")
@@ -9,9 +8,43 @@ module.exports = () => {
 //   })
 //   .catch(console.error);
 
-//Creamos la promesa 
-const mdLinks = (path) => {
+//Creamos la promesa MDLINKS
+const path = require('path');
+const fs = require('fs').promises;
+
+const mdLinks = (filePath) => {
   return new Promise((resolve, reject) => {
-   
+    // Verificamos que la ruta exista
+    fs.access(filePath)
+      .then(() => {
+        // La ruta existe, continuamos con el procesamiento
+        const absolutePath = path.resolve(filePath); // Convertimos la ruta a absoluta
+        console.log(absolutePath); // Mueve el console.log aquí
+        return fs.readFile(absolutePath, 'utf-8');
+      })
+      .then((content) => {
+        const links = [];
+        const linkRegex = /(\[([^\]]*)\]\(([^)]*)\))/g;
+
+        let match;
+        while ((match = linkRegex.exec(content)) !== null) {
+          const [, fullMatch, text, href] = match;
+          links.push({ href, text, file: filePath });
+        }
+
+        resolve(links);
+      })
+      .catch((error) => {
+        reject(`Error al leer el archivo: ${error.message}`);
+      });
   });
 };
+
+// Ejemplo de uso
+mdLinks('C:/Users/Yaya/Laboratoria/DEV010-md-links/lib/Readme.md')
+  .then((links) => {
+    console.table(links);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
---
+++ lib/Readme.md
@@ -0,0 +1,11 @@
+
+![pueba](https://www.youtube.com/watch?v=yxLOBFXSkv0&pp=ygUNdmVyY2VsIGRlcGxveQ%3D%3D)
+
+![pueba](https://www.youtube.com/watch?v=_2VHVIJCtGk&list=PL3aEngjGbYhnrRfZKMxzn79qdgPx)
+![pueba](https://www.youtube.com/watch?v=J3QUbmNk3Es q&AL7OWM&index=5)
+
+![pueba](https://jestjs.io/docs/getting-started)
+
+
+![prueba](https://curriculum.laboratoria.la/es/topics/javascript/arrays/arrays)
+![pueba](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array)
